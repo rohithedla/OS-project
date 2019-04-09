56db8c70fa16d1fba9b7da3b49140273a697a12d
rohithedla <49159392+rohithedla@users.noreply.github.com>
2019-04-09 09:57:43 +0530

    Add files via upload

---
+++ Rohithedla.c
@@ -0,0 +1,78 @@
+#include<stdio.h> 
+ 
+int main() 
+{ 
+ 
+  int count,i,j,n,time,remain,flag=0,time_quantum,pos,temp; 
+  int wait_time=0,turnaround_time=0,at[20],bt[20],rt[20],pr[20],p[20]; 
+  printf("Enter Total Process:\t "); 
+  scanf("%d",&n); 
+  remain=n; 
+  for(count=0;count<n;count++) 
+  { 
+    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
+    scanf("%d",&at[count]); 
+    scanf("%d",&bt[count]);
+	printf("Enter priority of process :");
+	scanf("%d",&pr[count]); 
+    rt[count]=bt[count]; 
+  } 
+  printf("Enter Time Quantum:\t"); 
+  scanf("%d",&time_quantum); 
+  //sorting burst time, priority and process number in ascending order using selection sort
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+                pos=j;
+        }
+ 
+        temp=pr[i];
+        pr[i]=pr[pos];
+        pr[pos]=temp;
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
+  for(time=0,count=0;remain!=0;) 
+  { 
+    if(rt[count]<=time_quantum && rt[count]>0) 
+    { 
+      time+=rt[count]; 
+      rt[count]=0; 
+      flag=1; 
+    } 
+    else if(rt[count]>0) 
+    { 
+      rt[count]-=time_quantum; 
+      time+=time_quantum; 
+    } 
+    if(rt[count]==0 && flag==1) 
+    { 
+      remain--; 
+      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]); 
+      wait_time+=time-at[count]-bt[count]; 
+      turnaround_time+=time-at[count]; 
+      flag=0; 
+    } 
+    if(count==n-1) 
+      count=0; 
+    else if(at[count+1]<=time) 
+      count++; 
+    else 
+      count=0; 
+  } 
+  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/n); 
+  printf("Avg Turnaround Time = %f",turnaround_time*1.0/n); 
+  
+  return 0; 
+}
+
